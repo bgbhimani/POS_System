fed6cf7e2a898780a3b420eef659d99cea8f7ad2
Bhavik Bhimani <166936122+bgbhimani@users.noreply.github.com>
2025-07-15 13:18:38 +0530

    basic setup of frontend

---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+
+
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-4 px-8 bg-[#1a1a1a]">
+      {/* LOGO */}
+      <div onClick={() => navigate("/")} className="flex items-center gap-2 cursor-pointer">
+        <img src={logo} className="h-8 w-8" alt="restro logo" />
+        <h1 className="text-lg font-semibold text-[#f5f5f5] tracking-wide">
+          POS System
+        </h1>
+      </div>
+
+
+      {/* SEARCH */}
+      <div className="flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5 py-2 w-[500px]">
+        <FaSearch className="text-[#f5f5f5]" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-[#1f1f1f] outline-none text-[#f5f5f5]"
+        />
+      </div>
+
+      {/* LOGGED USER DETAILS */}
+      <div className="flex items-center gap-4">
+        {/* {userData.role === "Admin" && (
+          <div onClick={() => navigate("/dashboard")} className="bg-[#1f1f1f] rounded-[15px] p-3 cursor-pointer">
+            <MdDashboard className="text-[#f5f5f5] text-2xl" />
+          </div>
+        )} */}
+        <div className="bg-[#1f1f1f] rounded-[15px] p-3 cursor-pointer">
+          <FaBell className="text-[#f5f5f5] text-2xl" />
+        </div>
+        <div className="flex items-center gap-3 cursor-pointer">
+          <FaUserCircle className="text-[#f5f5f5] text-4xl" />
+          <div className="flex flex-col items-start">
+            <h1 className="text-md text-[#f5f5f5] font-semibold tracking-wide">
+              {/* {userData.name || "TEST USER"} */}
+              Bhavik G Bhimani 
+            </h1>
+            <p className="text-xs text-[#ababab] font-medium">
+              {/* {userData.role || "Role"} */}
+              Admin
+            </p>
+          </div>
+          {/* <IoLogOut
+            onClick={handleLogout}
+            className="text-[#f5f5f5] ml-2"
+            size={40}
+          /> */}
+        </div>
+      </div>
+
+
+
+    </header>
+
+
+
+
+
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import BottomNav from "../components/shared/BottomNav";
+
+const Home = () => {
+    return (
+        <section className="bg-[#1f1f1f]  h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+
+            {/* Left Div */}
+            <div className="flex-[3] bg-red-400">
+                {/* <Greetings /> */}
+                {/* <div className="flex items-center w-full gap-3 px-8 mt-8">
+                    <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+                    <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6} />
+                </div> */}
+                {/* <RecentOrders /> */}
+            </div>
+
+
+
+
+            {/* Right Div */}
+            <div className="flex-[2] bg-blue-500 ">
+                {/* <PopularDishes /> */}
+            </div>
+            <BottomNav />
+
+
+        </section>
+    )
+}
+
+export default Home
